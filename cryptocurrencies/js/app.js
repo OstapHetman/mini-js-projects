6868692e8bca9d45e53dc4a66bff6af0f3f2c00a
OstapHetman <prickly4528@gmail.com>
2018-08-06 14:24:49 +0300

    reading values from the form

--- cryptocurrencies/js/app.js
+++ cryptocurrencies/js/app.js
@@ -1,4 +1,18 @@
 // Instantiate the class
-
 const cryptoApi = new CryptoAPI();
 const ui = new UI();
+
+// Create vars
+const form = document.getElementById("form");
+
+// Events
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  // read currency
+  const currencySelect = document.getElementById("currency").value;
+  // read crypticurrency
+  const cryptoCurrencySelect = document.getElementById("cryptocurrency").value;
+
+  console.log(`${currencySelect} : ${cryptoCurrencySelect}`);
+  // Validate selects
+});
