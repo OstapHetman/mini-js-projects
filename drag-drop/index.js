4a5441e4846a4c8134536dba49f93dc82f0bd0d1
OstapHetman <prickly4528@gmail.com>
2018-08-13 19:16:08 +0300

    finish project

--- drag-drop/index.js
+++ drag-drop/index.js
@@ -5,6 +5,14 @@ const empties = document.querySelectorAll(".empty");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
+//Loop through empties and call drag event
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
 // Drag Functions
 function dragStart() {
   this.className += " hold";
@@ -15,4 +23,19 @@ function dragStart() {
 
 function dragEnd() {
   this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hover";
+}
+function dragLeave() {
+  this.className = "empty";
+}
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
 }
