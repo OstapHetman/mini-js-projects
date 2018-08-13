ce18bac24e10c238abd94edf8c26be19abd9600d
OstapHetman <prickly4528@gmail.com>
2018-08-13 18:59:54 +0300

    add basic js code

---
+++ drag-drop/index.js
@@ -0,0 +1,11 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+// Fill Listeners
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+// Drag Functions
+function dragStart() {}
+
+function dragEnd() {}
