4d0acd430cb334d8606c468a147ea36c65875a3b
OstapHetman <prickly4528@gmail.com>
2018-08-16 14:03:59 +0300

    setup firestore, setup app with vue-cli, create firebase config files

---
+++ employee-manager/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyATkJ25380s4b5PeMipeNxNNAcYWTMuOqs",
+  authDomain: "vuefs-dev-6560a.firebaseapp.com",
+  databaseURL: "https://vuefs-dev-6560a.firebaseio.com",
+  projectId: "vuefs-dev-6560a",
+  storageBucket: "vuefs-dev-6560a.appspot.com",
+  messagingSenderId: "331688615371"
+};
