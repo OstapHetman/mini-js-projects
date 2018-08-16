b5a50e8c66aa2d5d64252dc3814ceedb93f926c7
OstapHetman <prickly4528@gmail.com>
2018-08-16 11:35:55 +0300

    get docs from firestore

--- reading-data-from-firebase/js/app.js
+++ reading-data-from-firebase/js/app.js
@@ -9,6 +9,34 @@ const config = {
 };
 
 firebase.initializeApp(config);
-
 const db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true });
+
+// Start coding
+const cafeList = document.querySelector("#cafe-list");
+
+// Create element and render cafe
+function renderCafe(doc) {
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let city = document.createElement("span");
+
+  li.setAttribute("data-id", doc.id);
+  li.className = "list-group-item";
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+
+  li.appendChild(name);
+  li.appendChild(city);
+
+  cafeList.appendChild(li);
+}
+
+db.collection("cafes")
+  .get()
+  .then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      // console.log(doc.data());
+      renderCafe(doc);
+    });
+  });
