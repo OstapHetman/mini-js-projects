1b00cc4cf6921dd8b510c8028a10647506b91126
OstapHetman <prickly4528@gmail.com>
2018-08-16 16:12:39 +0300

    delete dev files

--- employee-manager/src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyATkJ25380s4b5PeMipeNxNNAcYWTMuOqs",
-  authDomain: "vuefs-dev-6560a.firebaseapp.com",
-  databaseURL: "https://vuefs-dev-6560a.firebaseio.com",
-  projectId: "vuefs-dev-6560a",
-  storageBucket: "vuefs-dev-6560a.appspot.com",
-  messagingSenderId: "331688615371"
-};
