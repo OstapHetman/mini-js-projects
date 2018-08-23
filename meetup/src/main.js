1dadc36b6a275fe023b3032fa1d4be54575dc313
OstapHetman <prickly4528@gmail.com>
2018-08-23 16:23:48 +0300

    connect firebase, signup view and form

--- meetup/src/main.js
+++ meetup/src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App";
+import * as firebase from "firebase";
 import router from "./router";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
@@ -16,5 +17,15 @@ new Vue({
   el: "#app",
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyATkJ25380s4b5PeMipeNxNNAcYWTMuOqs",
+      authDomain: "vuefs-dev-6560a.firebaseapp.com",
+      databaseURL: "https://vuefs-dev-6560a.firebaseio.com",
+      projectId: "vuefs-dev-6560a",
+      storageBucket: "vuefs-dev-6560a.appspot.com",
+      messagingSenderId: "331688615371"
+    });
+  }
 });
