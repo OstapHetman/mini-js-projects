310ecfecf0bd064d183adfe6f5e9e74e79e64f6c
OstapHetman <prickly4528@gmail.com>
2018-08-24 22:18:31 +0300

    post tasks to json-server

--- todo/js/main.js
+++ todo/js/main.js
@@ -5,37 +5,45 @@ const allTasks = document.getElementById("list-group");
 
 // Events
 document.addEventListener("DOMContentLoaded", fetchFromJson);
-// form.addEventListener("submit", formSubmitted);
+form.addEventListener("submit", formSubmitted);
 
 // Functions
-// function formSubmitted(e) {
-//   e.preventDefault();
-
-//   let inputData = input.value;
-//   fetch("todos.json", {
-//     method: "POST",
-//     body: JSON.stringify({ task: inputData })
-//   })
-//     .then(resp => {
-//       console.log("Done...");
-//     })
-//     .catch(err => {
-//       console.log(`[Err ${err}]`);
-//     });
-// }
+function formSubmitted(e) {
+  e.preventDefault();
+  let data = {
+    title: input.value
+  };
+  let options = {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  fetch("http://localhost:3000/todos", options)
+    .then(response => {
+      response.json;
+      console.log("Done...");
+    })
+    .catch(err => {
+      console.log(`[Err ${err}]`);
+    });
+}
 
 function fetchFromJson() {
   let output = "";
-  fetch("todos.json")
+  fetch("http://localhost:3000/todos")
     .then(response => response.json())
     .then(tasks => {
-      let data = tasks.todos;
-      data.forEach(task => {
+      tasks.forEach(task => {
         // console.log(task.title);
         output += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
+        <li class="list-group-item d-flex justify-content-between align-items-center" data-id="${
+          task.id
+        }">
         ${task.title}
-        <i class="far fa-trash-alt text-danger"></i>
+        <i class="far fa-trash-alt text-danger" style="cursor: pointer"></i>
         </li>
         `;
       });
