89d911b8fa66df1a82fccf790d0b261302f94fce
OstapHetman <prickly4528@gmail.com>
2018-08-26 21:26:01 +0300

    get select option value and get all brands & push to arr

--- sorting-items/app.js
+++ sorting-items/app.js
@@ -1,24 +1,24 @@
 const listItems = document.querySelector("#list-items");
 const select = document.querySelector("#brands");
-const selected = document.querySelector("#selected");
 
+// Show Brands from DB
 document.addEventListener("DOMContentLoaded", fetchFromJson);
 
 // Get
 function fetchFromJson() {
   let output = "";
-  //  let selectEl = "";
 
   fetch("http://localhost:3000/brands")
     .then(response => response.json())
     .then(brands => {
       brands.forEach(brand => {
         output += `
-                <div class="col-md-4 mb-3 ">
-                    <div class="border text-center">
-                        <p class="mb-0">${brand.brand}</p>
-                    </div>    
-                </div>
+            <div class="col-md-4 mb-3 brand ${brand.brand.toLowerCase()}" 
+            data-brand="${brand.brand}">
+                <div class="border text-center">
+                    <p class="mb-0" >${brand.brand}</p>
+                </div>    
+            </div>
         `;
         let option = document.createElement("option");
         option.value = brand.brand;
@@ -29,3 +29,36 @@ function fetchFromJson() {
     })
     .catch(err => console.log(err));
 }
+
+setTimeout(function sorting() {
+  let cols = document.querySelectorAll(".brand");
+  let options = select.querySelectorAll("option");
+  let arr = [];
+
+  cols.forEach(el => {
+    let attr = el.getAttribute("data-brand");
+
+    arr.push(attr);
+  });
+
+  select.addEventListener("change", function() {
+    // let a = e.target.querySelector("option");
+    // a.forEach(doc => {
+    console.log(this.value);
+    // });
+  });
+
+  //   options.forEach(el => {
+  //     // console.log(`[${el}]`);
+
+  //     el.addEventListener("click", e => {
+  //       console.log("e.target");
+  //     });
+  //   });
+
+  function a(element) {
+    return (element = "Audi");
+  }
+
+  //   console.log(arr.find(a));
+}, 1000);
