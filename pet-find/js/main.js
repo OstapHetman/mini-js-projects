78ec9d16e5f4a8a9caebd37ecc4e8df5e25166b1
OstapHetman <prickly4528@gmail.com>
2018-09-12 12:12:00 +0300

    fetch all thing in left side

--- pet-find/js/main.js
+++ pet-find/js/main.js
@@ -36,12 +36,33 @@ function showAnimals(pets) {
   results.innerHTML = "";
   // Loop Through Pets
   pets.forEach(pet => {
+    console.log(pet);
     const div = document.createElement("div");
     div.classList.add("card", "card-body", "mb-3");
     div.innerHTML = `
         <div class="row">
             <div class="col-sm-6">
                 <h4>${pet.name.$t} (${pet.age.$t})</h4>
+                <p class="text-secondary">${pet.breeds.breed.$t}</p>
+                <p>
+                ${pet.contact.address1.$t} ${pet.contact.city.$t}
+                ${pet.contact.state.$t} ${pet.contact.zip.$t}
+                </p>
+                <ul class="list-group">
+                    <li class="list-group-item">Phone: ${
+                      pet.contact.phone.$t
+                    }</li>
+                    ${
+                      pet.contact.email.$t
+                        ? `<li class="list-group-item">Email: ${
+                            pet.contact.email.$t
+                          }</li>`
+                        : ``
+                    }
+                    <li class="list-group-item">Shelter ID: ${
+                      pet.shelterId.$t
+                    }</li>
+                </ul>
             </div>
             <div class="col-sm-6">
             </div>
