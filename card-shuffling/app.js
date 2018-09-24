d6e8ffe33ceee2dbfc2d6ff81ff87230e95a63a5
OstapHetman <prickly4528@gmail.com>
2018-09-24 12:34:38 +0300

    display initial state of cards

---
+++ card-shuffling/app.js
@@ -0,0 +1,29 @@
+new Vue({
+  el: "#app",
+  data: {
+    ranks: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+    suits: ["♥", "♦", "♠", "♣"],
+    cards: []
+  },
+  created() {
+    this.displayInitialDeck();
+  },
+  methods: {
+    displayInitialDeck() {
+      let id = 1;
+      this.cards = [];
+
+      for (let s = 0; s < this.suits.length; s++) {
+        for (let r = 0; r < this.ranks.length; r++) {
+          let card = {
+            id: id,
+            rank: this.ranks[r],
+            suit: this.suits[s]
+          };
+          this.cards.push(card);
+          id++;
+        }
+      }
+    }
+  }
+});
