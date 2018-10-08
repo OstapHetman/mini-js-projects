db688c5a66ef971e2ca9cd6b8bdde07ff86ff4af
OstapHetman <prickly4528@gmail.com>
2018-10-08 18:11:57 +0300

    display countdown in ui

--- comming-soon/index.js
+++ comming-soon/index.js
@@ -1,4 +1,4 @@
-const countdown = document.querySelector(".countdown");
+const countdown = document.querySelector(".counter");
 
 const launchDate = new Date("Jan 1, 2019, 13:00:00").getTime();
 
@@ -17,4 +17,10 @@ const interval = setInterval(() => {
   );
   const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display result
+  countdown.querySelector("#days h4").innerHTML = days;
+  countdown.querySelector("#hours h4").innerHTML = hours;
+  countdown.querySelector("#minutes h4").innerHTML = mins;
+  countdown.querySelector("#seconds h4").innerHTML = seconds;
 }, 1000);
